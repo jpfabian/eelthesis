33d80d3589623e5fba560cd35f2cf067b4d6c416
jpfabian <jpfabian24@gmail.com>
2025-10-25 10:26:59 +0800

    commit

--- js-back-end/server.js
+++ js-back-end/server.js
@@ -21,9 +21,9 @@ app.use(cors({ origin: ["http://localhost:3000", "http://127.0.0.1:5501"], crede
 app.use(bodyParser.json());
 
 // 3️⃣ Serve frontend static files
-app.use(express.static(path.join(__dirname, 'js-front-end')));
+app.use(express.static(path.join(__dirname, 'index.html')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'js-front-end', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // 4️⃣ MySQL pool
