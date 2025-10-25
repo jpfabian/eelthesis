7fff71eb6024679b3230a855c0044014bb731a62
jpfabian <jpfabian24@gmail.com>
2025-10-25 10:30:21 +0800

    update

--- js-back-end/server.js
+++ js-back-end/server.js
@@ -21,7 +21,7 @@ app.use(cors({ origin: ["http://localhost:3000", "http://127.0.0.1:5501"], crede
 app.use(bodyParser.json());
 
 // 3️⃣ Serve frontend static files
-app.use(express.static(path.join(__dirname, 'index.html')));
+app.use(express.static(path.join(__dirname, '/')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
