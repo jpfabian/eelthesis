1661bb9b48027c225870f447c22cbbfc1e957b2f
jpfabian <jpfabian24@gmail.com>
2026-02-22 13:08:55 +0800

    init

--- js-front-end/api-config.js
+++ js-front-end/api-config.js
@@ -1,4 +1,10 @@
-
-if (typeof window !== "undefined" && window.API_BASE === undefined) {
-  window.API_BASE = "";
+// Use same-origin for API when not on loopback to avoid Private Network Access block
+// (e.g. page at http://100.52.238.165 must not fetch http://localhost:3000)
+if (typeof window !== "undefined") {
+  var isLoopback = /^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/i.test(window.location.origin);
+  if (!isLoopback) {
+    window.API_BASE = "";
+  } else if (window.API_BASE === undefined) {
+    window.API_BASE = "";
+  }
 }
